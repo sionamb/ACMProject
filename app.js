6db3af58c20293f27ea259694747d9a60465ec88
Siona Beaudoin <smbeaudo@svsu.edu>
2023-03-21 16:06:15 -0400

    changes

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 let minutes = 30
 
+const countdownComponent = document.querySelector('#Time');
+countdownComponent.setCountdownLength([0,0,30]);
+countdownComponent.startCountdown();
+
+console.log(countdownComponent)
