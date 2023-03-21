c28fd8a779b81744ad6bc1237c8e12178e4bd1e1
Bennet17 <70712859+Bennet17@users.noreply.github.com>
2023-03-21 15:54:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
+const moment  = require('moment')
+const timer = require("moment-timer")
+
 let minutes = 30
 
+var time = moment.duration(minutes, "seconds").timer({loop: true}, function() {
+    
+    // Callback
+});
+
+console.log(time)
