6a5bb817c612299e1655409afd1f0c237b170266
Siona Beaudoin <smbeaudo@svsu.edu>
2023-04-04 15:41:53 -0400

    put timer in start function

--- ideas/laundry.js
+++ ideas/laundry.js
@@ -10,31 +10,40 @@
 // Get millisecond space. 
 const timerElement = document.querySelector('div#container main.main div#maintimer span.millisecond')
 
+let flag = false
 
-// Set the time limit in minutes
-var timeLimit = 45;
+function startTimer(){
+  // Set the time limit in minutes
+  var timeLimit = 45;
 
-// Convert minutes to seconds
-var timeInSeconds = timeLimit * 60;
+  // Convert minutes to seconds
+  var timeInSeconds = timeLimit * 60;
 
-// Get the timer element from the HTML
-//var timerElement = document.getElementById('timer');
+  // Get the timer element from the HTML
+  //var timerElement = document.getElementById('timer');
 
-// Update the timer every second
-var countdown = setInterval(function() {
-  // Calculate minutes and seconds
-  var minutes = Math.floor(timeInSeconds / 60);
-  var seconds = timeInSeconds - (minutes * 60);
+  // Update the timer every second
+  var countdown = setInterval(function() {
+    // Calculate minutes and seconds
+    var minutes = Math.floor(timeInSeconds / 60);
+    var seconds = timeInSeconds - (minutes * 60);
 
-  // Display the time in minutes and seconds
-  timerElement.innerHTML = minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+    // Display the time in minutes and seconds
+    timerElement.innerHTML = minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
 
-  // Decrement time by one second
-  timeInSeconds--;
+    // Decrement time by one second
+    timeInSeconds--;
 
-  // Check if time has run out
-  if (timeInSeconds < 0) {
-    clearInterval(countdown);
-    timerElement.innerHTML = "Time's up!";
+    // Check if time has run out
+    if (timeInSeconds < 0) {
+      clearInterval(countdown);
+      timerElement.innerHTML = "Time's up!";
+    }
+  }, 1000);
+
+  function pauseTimer(){
+    flag = true
   }
-}, 1000);
+
+}
+
